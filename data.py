a9777d7279ade2573f46912052573f2dd5ef75ff
Mariavellous <mm.alcaide38@gmail.com>
2022-06-29 22:25:37 -0700

    retrieve a list of all questions

--- data.py
+++ data.py
@@ -3,7 +3,7 @@ import requests
 
 
 parameters = {
-    "amount": 1,
+    "amount": 3,
     # Category is Entertainment: Film
     "category": 11,
     "type": "multiple",
@@ -15,5 +15,19 @@ response = requests.get("https://opentdb.com/api.php", params=parameters)
 response.raise_for_status()
 # retrieve the data in json format
 data = response.json()
-print(data)
+
+# Retrieve a list of all questions
+question_data = data["results"]
+print(question_data)
+
+# Parse all necessary info from 0th index question.
+question = data["results"][0]["question"]
+print(question)
+correct_answer = data["results"][0]["correct_answer"]
+print(correct_answer)
+incorrect_answers = data["results"][0]["incorrect_answers"]
+print(incorrect_answers)
+
+
+
 
