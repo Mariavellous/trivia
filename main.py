6d725ee8cd891de78d44c21bd5301163c4a7e9d5
Mariavellous <mm.alcaide38@gmail.com>
2022-07-02 00:25:23 -0700

    start to create trivia-collection database in sqlite

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 import html
 from data import question_data
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import CheckConstraint
+
+app = Flask(__name__)
+# create database in sqlite for now
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///trivia-collection.db'
+
+db = SQLAlchemy(app)
+
+class Player(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(30), db.CheckConstraint('first_name > 0'), nullable=False)
+    last_name = db.Column(db.String(30), db.CheckConstraint('last_name > 0'), nullable=False)
+    # email_address VARCHAR(50) UNIQUE NOT NULL CHECK(email_address LIKE '%@%.%'),
+    email_address = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(), db.CheckConstraint('password > 0'), unique=True, nullable=False)
 
 def main():
     for question in question_data:
         question_q = question["question"]
-        # decdde the HTML characters to its original character representation
+        # decode the HTML characters to its original character representation
         question_text = html.unescape(question_q)
         print(question_text)
         question_answer = question["correct_answer"]
@@ -13,6 +30,7 @@ def main():
         print(incorrect_answers)
 
 # TODO: Create a database for the question.
+
 
 if __name__ == "__main__":
     main()
