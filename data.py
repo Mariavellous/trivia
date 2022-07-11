b0dd931b47406ab5d87fe9172a2c0ee74ea6fba1
Mariavellous <mm.alcaide38@gmail.com>
2022-07-11 10:44:26 -0700

    Randomnly include correct answer to the list of incorrect answer to create a choices list

--- data.py
+++ data.py
@@ -1,9 +1,11 @@
 # requests module allows you to send HTTP requests. Install requests module.
 import requests
+import html
+import random
 
 
 parameters = {
-    "amount": 3,
+    "amount": 1,
     # Category is Entertainment: Film
     "category": 11,
     "type": "multiple",
@@ -18,17 +20,33 @@ data = response.json()
 
 
 # Retrieve a list of all questions
-question_data = data["results"]
+question_data = data["results"][0]
 # print(question_data)
 
-# Parse all necessary info from 0th index question.
-# question = data["results"][0]["question"]
-# print(question)
-# correct_answer = data["results"][0]["correct_answer"]
-# print(correct_answer)
-# incorrect_answers = data["results"][0]["incorrect_answers"]
-# print(incorrect_answers)
+def get_question(question_data):
+    question = question_data["question"]
+    print(question)
+    correct_answer = question_data["correct_answer"]
+    print(correct_answer)
+    choices = question_data["incorrect_answers"]
+    # Randomnly add the correct answer to the list of incorrect answers
+    integer = random.randint(0, 3)
+    print(integer)
+    choices.insert(integer, correct_answer)
+    print(choices)
 
+print(get_question(question_data))
+
+
+# Parse all necessary info
+# class Trivia:
+#     def __init__(self, category, type, difficulty, question, correct_answer, incorrect_answer):
+#         self.category = category
+#         self.type = type
+#         self.difficulty = difficulty
+#         self.question = question
+#         self.correct_answer = correct_answer
+#         self.incorrect_answer = incorrect_answer
 
 
 
--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 import html
-
-import data
 from data import question_data
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import CheckConstraint
-from data import *
 
 app = Flask(__name__)
 # create database in sqlite for now
@@ -27,17 +24,17 @@ class Player(db.Model):
 
 class Question(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String(200), db.CheckConstraint('text > 0'), nullable=False)
-    correct_answer = db.Column(db.String(100), db.CheckConstraint('correct_answer > 0'), nullable=False)
+    text = db.Column(db.String(5000), db.CheckConstraint('text > 0'), nullable=False)
+    correct_answer = db.Column(db.String(1000), db.CheckConstraint('correct_answer > 0'), nullable=False)
     # this is a string of data object
-    choices = db.Column(db.String(100), db.CheckConstraint('choices > 0'), nullable=False)
+    choices = db.Column(db.String(1000), db.CheckConstraint('choices > 0'), nullable=False)
 
 
 class Guess(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     player_id = db.Column(db.Integer, db.ForeignKey(Player.id), nullable=False)
     question_id = db.Column(db.Integer, db.ForeignKey(Question.id), nullable=False)
-    player_answer = db.Column(db.String(100), db.CheckConstraint('player_answer > 0'), nullable=False)
+    player_answer = db.Column(db.String(1000), db.CheckConstraint('player_answer > 0'), nullable=False)
     played_on = db.Column(db.Date)
     result = db.Column(db.Boolean, nullable=False)
 
@@ -46,35 +43,43 @@ db.create_all()
 db.session.commit()
 
 # TODO: what are my REST API request.
-# TODO: Make a get request to access trivia question for trivia API
+
+new_trivia = Question(text=question_data["question"], correct_answer=question_data["correct_answer"],
+                      choices="3")
+db.session.add(new_trivia)
+db.session.commit()
 
 
 def main():
-    for question in question_data:
-        question_q = question["question"]
-        # decode the HTML characters to its original character representation
-        question_text = html.unescape(question_q)
-        print(question_text)
-        question_answer = question["correct_answer"]
-        print(question_answer)
-        incorrect_answers = question["incorrect_answers"]
-        print(incorrect_answers)
+    # for question in question_data:
+    #     question_q = question["question"]
+    #     # decode the HTML characters to its original character representation
+    #     question_text = html.unescape(question_q)
+    #     print(question_text)
+    #     question_answer = question["correct_answer"]
+    #     print(question_answer)
+    #     incorrect_answers = question["incorrect_answers"]
+    #     print(incorrect_answers)
 
-def get_trivia_question:
-    question = data.question_data()
-    return question
-
-
+    print(question_data)
 
 
 
 # Render the main template
-@app.route('/')
-def home():
-    return render_template('index.html')
+# @app.route('/')
+# def home():
+#     return render_template('index.html')
 
-def show_trivia('/trivia', methods=['GET']):
-    pass
+# def show_trivia('/trivia', methods=['POST']):
+    # Fetch the question/answer from API.
+    # Update the QUESTION TABLE
+    # new_trivia = Question(text=question_data["question"], correct_answer=question_data["correct_answer"],
+    #                       choices=question_data["incorrect_answer"])
+    # db.session.add(new_trivia)
+    # db.session.commit()
+
+
+
 
 
 # TODO: Connect to heroku.
